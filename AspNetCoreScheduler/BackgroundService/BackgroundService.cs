4ec2586dc7afea704e4f2f41f875486204827097
FeryazHacimuradov <tu201906125@code.edu.az>
2024-02-06 12:30:37 +0400

    Add project files.

---
+++ AspNetCoreScheduler/BackgroundService/BackgroundService.cs
@@ -0,0 +1,48 @@
+﻿
+namespace AspNetCoreScheduler.BackgroundService
+{
+    public abstract class BackgroundService : IHostedService
+    {
+        private Task _executingTask;
+        private readonly CancellationTokenSource _stoppingCts = new CancellationTokenSource();
+        public virtual Task StartAsync(CancellationToken cancellationToken)
+        {
+            _executingTask = ExecuteAsync(_stoppingCts.Token);
+
+            if (_executingTask.IsCompleted)
+            {
+                return _executingTask;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (_executingTask == null)
+            {
+                return;
+            }
+            try
+            {
+                _stoppingCts.Cancel();
+            }
+            finally
+            {
+                await Task.WhenAny(_executingTask, Task.Delay(Timeout.Infinite, cancellacationToken));
+            }
+        }
+
+        protected virtual async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            do
+            {
+                await Process();
+
+                await Task.Delay(5000, stoppingToken);
+            } while (!stoppingToken.IsCancellationRequested);
+        }
+
+        protected abstract Task Process();
+    }
+}
